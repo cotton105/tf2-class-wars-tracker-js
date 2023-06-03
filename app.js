dba1b333d33b9ea1821ee220b968e801b198ae09
Luke Cotton <luke@cotton.org.uk>
2023-06-03 13:10:48 +0100

    Refactor Express callback functions

--- app.js
+++ app.js
@@ -10,26 +10,31 @@ const listenPort = 3000;
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
+app.use(recordConnection);
+app.get('/', renderHomepage);
+app.all('*', catchPageNotFound);
+
+app.listen(listenPort, () => {
+    log.info(`Server listening on http://${listenUrl}:${listenPort}.`);
+});
+
+
+function recordConnection(req, res, next) {
     let connectingIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     log.info(`Connection to ${req.url} from ${connectingIp}`);
     next();
-});
+}
 
-app.get('/', (req, res) => {
+function renderHomepage(req, res) {
     let options = {
         title: 'APP',
         message: 'Hello world!'
     };
     res.render('index', options);
-});
+}
 
-app.all('*', (req, res, next) => {
+function catchPageNotFound(req, res, next) {
     log.info(`${req.url} not found.`);
     res.status(404).send('Error');
     next();
-});
-
-app.listen(listenPort, () => {
-    log.info(`Server listening on http://${listenUrl}:${listenPort}.`);
-});
+}
