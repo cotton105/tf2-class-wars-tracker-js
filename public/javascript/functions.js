07316ce21af107638c50c05cf92c0f618f1f3473
Luke Cotton <luke@cotton.org.uk>
2023-06-03 14:09:31 +0100

    Add "record win" buttons to merc select grids

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
-
     $('#tracking-grid td').on('click', matchupTableClickHandler);
-
 });
+
+const selectedMercs = {
+    blu: null,
+    red: null
+};
 
 function matchupTableClickHandler() {
     let parents = $(this).data('parents');
-    console.log(parents);
+    selectedMercs.blu = parents.blu;
+    selectedMercs.red = parents.red;
+    console.log(selectedMercs);
 }
