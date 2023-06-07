65284698ac577d588cf66306ff47c14efc3295c6
Luke Cotton <luke@cotton.org.uk>
2023-06-07 14:34:18 +0100

    #5 Correct calculation for bias

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -1,6 +1,11 @@
 function calculateBias(x, y) {
-    if (x != 0 && y == 0)
+    if (x != 0 && y == 0) {
         return -1;
-    else
+    }
+    else if (y == 0) {
+        return 0;
+    }
+    else {
         return (1 - x / y) / (1 + x / y);
+    }
 }
