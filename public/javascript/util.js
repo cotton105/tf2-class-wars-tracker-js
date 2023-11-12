5adfbc7c8d3404a69884777d9287fafbfe127b6f
Luke Cotton <luke@cotton.org.uk>
2023-11-12 22:53:55 +0000

    #15 Move cookie manipulation to `util.js` Also add `deleteCookie()` function, and a function for updating user selections

--- public/javascript/util.js
+++ public/javascript/util.js
@@ -20,3 +20,38 @@ function emptyObject(obj) {
     }
     return obj;
 }
+
+/**
+ * Set a value to a cookie.
+ * @param {string} cname Name of the cookie to set.
+ * @param {any} cvalue Value to set for the cookie.
+ * @param {number} exdays How many days the cookie should persist for.
+ */
+function setCookie(cname, cvalue, exdays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = `${cname}=${cvalue};${expires};path=/`;
+}
+
+/**
+ * Gets the value of the specified cookie.
+ * @param {string} cname Name of the cookie to read.
+ * @returns Value of the specified cookie.
+ */
+function getCookie(cname) {
+    const decodedCookies = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookies.split(';');
+    const valRegExp = /=([^;]+)/;
+    const targetCookie = cookieArray.find((element) => element.includes(`${cname}=`));
+    const result = targetCookie && valRegExp.exec(targetCookie)[1];
+    return result;
+}
+
+/**
+ * Delete a cookie given its name.
+ * @param {string} cname Name of the cookie to delete.
+ */
+function deleteCookie(cname) {
+    document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
