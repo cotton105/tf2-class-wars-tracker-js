61654ee4308292a25765b0ded2c8633a913f461b
Luke Cotton <luke@cotton.org.uk>
2023-11-12 21:15:05 +0000

    #15 Add functions for manipulating cookies

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -193,3 +193,30 @@ function toggleAllGameModes() {
     selected.gameMode = checked ? null : $('#select-game-mode option:selected').data('game-mode-id');
     refreshMatchupGrid();
 }
+
+/**
+ * Set a value to a cookie.
+ * @param {string} cname Name of the cookie to set.
+ * @param {any} cvalue Value to set for the cookie.
+ * @param {number} exdays How many days the cookie should persist for.
+ */
+function setCookie(cname, cvalue, exdays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = `${cname}=${cvalue};${expires};path=/`;
+}
+
+/**
+ * Gets the value of the specified cookie.
+ * @param {string} cname Name of the cookie to read.
+ * @returns Value of the specified cookie.
+ */
+function getCookie(cname) {
+    const decodedCookies = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookies.split(';');
+    const valRegExp = /(?<==).?/;
+    const targetCookie = cookieArray.find((element) => element.includes(`${cname}=`));
+    const result = targetCookie && valRegExp.exec(targetCookie)[0];
+    return result;
+}
