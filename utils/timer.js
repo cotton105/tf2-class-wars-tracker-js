50e7bbe393a299f91e4c46a482e6db677d778e40
Luke Cotton <luke@cotton.org.uk>
2023-11-13 18:23:29 +0000

    #31 Add more comments to `timer.js`

--- utils/timer.js
+++ utils/timer.js
@@ -2,6 +2,12 @@ const crypto = require('crypto');
 
 const timers = {};
 
+/**
+ * Starts a new timer.
+ * @returns ID for the timer - pass this value to `stop()` or `split()`.
+ * @see `stop()`
+ * @see `split()`
+ */
 function start() {
      let idHash;
      while (true) {
@@ -14,16 +20,33 @@ function start() {
      return idHash;
 }
 
+/**
+ * Get the duration of the specified timer.
+ * @param {string} id ID of the timer to stop.
+ * @param {boolean} remove Whether to halt timing after read.
+ * @returns The measured time in seconds.
+ * @see `start()`
+ */
 function _stop(id, remove=true) {
     const duration = process.hrtime(timers[id]);
     if (remove) delete timers[id];
     return duration[0] + duration[1] / 10**9;
 }
 
+/**
+ * Stop the specified timer and get the duration.
+ * @param {string} id ID of the timer to read.
+ * @returns The measured time in seconds.
+ */
 function stop(id) {
     return _stop(id);
 }
 
+/**
+ * Get the duration of the specified timer and keep the timer going.
+ * @param {string} id ID of the timer to read.
+ * @returns The measured time in seconds.
+ */
 function split(id) {
     return _stop(id, false);
 }
